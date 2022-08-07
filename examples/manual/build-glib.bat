d2005ec39d68912dc2483235e280d28dcaf321ad
Jack Heaffey <jack.heaffey@atlantictechnology.co.uk>
2022-08-07 12:09:40 +0100

    wip

--- examples/manual/build-glib.bat
+++ examples/manual/build-glib.bat
@@ -1,13 +1,16 @@
-@REM cd pcre2-10.40
-@REM mkdir build
-@REM cd build
-@REM cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=..\pcre2-install ..
-@REM cmake --build .
-@REM cmake --install .
+cd pcre2-10.37
+mkdir build
+cd build
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=..\pcre2-install  ..
+cmake --build .
+cmake --install .
 
-@REM cd ..\..\
+cd ..\..\
 
 cd zlib-1.2.12
+@REM need to apply cmamke_dont_build_more_than_needed.patch from vpckg so that output lib is zlib.lib not zlibstatic.lib, as glib expects zlib.lib
+@REM actually also had to remove the if(UNIX) so that on windows it is called z.lib
+@REM  actually modifications to zlib may not be necessary if using cmake_prefix_path to find zlib instead
 mkdir build
 cd build
 cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=..\zlib-install ..
@@ -31,8 +34,11 @@ cd ..\..\
 cd gettext-0.21
 @REM  see build-gettext.sh
 
-set PKG_CONFIG_PATH=%cd%\pcre2-10.40\pcre2-install\lib\pkgconfig;%cd%\zlib-1.2.12\zlib-install\share\pkgconfig;%cd%\libffi-meson-3.2.9999.3\libffi-install/lib\pkgconfig
+@REM set PKG_CONFIG_PATH=%cd%\pcre2-10.37\pcre2-install\lib\pkgconfig;%cd%\zlib-1.2.12\zlib-install\share\pkgconfig;%cd%\libffi-meson-3.2.9999.3\libffi-install/lib\pkgconfig
+set PKG_CONFIG_PATH=%cd%\pcre2-10.37\pcre2-install\lib\pkgconfig;%cd%\libffi-meson-3.2.9999.3\libffi-install/lib\pkgconfig
+
 @REM note that pkg-config and gettext binaries were fetched from https://stackoverflow.com/a/22363820
+set CMAKE_PREFIX_PATH=%cd%\zlib-1.2.12\zlib-install
 @REM set PATH=%cd%\bin;%PATH%
 set PATH=%cd%\bin;%cd%\gettext-runtime\bin;%PATH%
 set INCLUDE=%INCLUDE%;%cd%\gettext\include
